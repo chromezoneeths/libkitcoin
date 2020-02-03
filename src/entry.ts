575baa8f3dd58ddff6737de0f213aeaacfa6e388
Tucker Carlson-Huber <tucker61761@gmail.com>
2020-02-03 10:41:37 -0600

    Added a config file

--- src/entry.ts
+++ src/entry.ts
@@ -1,6 +1,7 @@
 // This file is the entrypoint.
 const api = require( './api' );
 const helpers = require( './helpers' );
+const cfg = require( '../config.jsonc' );
 // Require libraries to standardize ws and fetch
 const ws = require( 'isomorphic-ws' );
 const rq = require( 'isomorphic-fetch' );
