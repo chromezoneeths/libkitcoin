c30925a2789a75e3ed3440bce847b6f1e1367ce7
Tucker Carlson-Huber <tucker61761@gmail.com>
2020-02-03 10:19:31 -0600

    Wrote the beginnings of a library

---
+++ src/abstractions.ts
@@ -0,0 +1,3 @@
+// Require libraries to standardize ws and fetch
+const ws = require( 'isomorphic-ws' );
+const rq = require( 'isomorphic-fetch' );
---
+++ src/entry.ts
@@ -0,0 +1,3 @@
+// This file does not contain any API code. You should look in abstractions.ts
+const abs = require( './abstractions' );
+
