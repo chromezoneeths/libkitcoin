2b373a1128830cc0efb63c0ed83c0f1e94f8f324
Tucker Carlson-Huber <tucker61761@gmail.com>
2020-02-03 10:41:44 -0600

    Added a ping function

--- src/api.ts
+++ src/api.ts
@@ -1,1 +1,12 @@
 // This file contains low-level API functions to directly access endpoints.
+
+function ping() { // Checks for an HTTP response at baseUrl/ping
+ return new Promise( async resolve => {
+  await fetch( `${cfg.baseUrl}/ping` ).then( ( res ) => {
+   resolve( res.ok ); // Resolves if the request was successful
+  } ).catch( () => {
+   resolve( false ); // `fetch` rejects on network failure
+  } );
+ } );
+}
+exports.ping = ping;
