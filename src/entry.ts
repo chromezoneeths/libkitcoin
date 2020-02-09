60586a0e87fa43fff992c3563175dbbf440cf0d3
Tucker Carlson-Huber <tucker61761@gmail.com>
2020-02-09 13:35:09 -0600

    Did more work on the reference library

--- src/entry.ts
+++ src/entry.ts
@@ -5,6 +5,7 @@ const cfg = require( '../config' );
 // Require libraries to standardize ws and fetch
 const ws = require( 'isomorphic-ws' );
 const rq = require( 'isomorphic-fetch' );
+export let persist; // You can save this and re-set it later for persistance
 
 exports = helpers;
 exports.api = api;
